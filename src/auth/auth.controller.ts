eb31006da38faae40cca1f2c8dc66f6124346fc7
Gustavo Adolfo Olmedo <gustavo.olmedo.formosa@gmail.com>
2025-04-12 14:48:08 +0100

    add class validator package, register dto & update auth controller register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 
 import { UsersService } from 'src/users/users.service';
+import { RegisterDto } from './models/register.dto';
 
 @Controller()
 export class AuthController {
   constructor(private usersService: UsersService) {}
 
   @Post('register')
-  async register(@Body() body) {
+  async register(@Body() body: RegisterDto) {
+    if (body.password !== body.passwordConfirm) {
+      throw new BadRequestException('Password do not match!');
+    }
     const hashedPassword = await bcrypt.hash(body.password, 12);
     return this.usersService.create({
       firstName: body.firstName,
