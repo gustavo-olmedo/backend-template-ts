cf8a42e5276b6a99c2251c1fee2c5cf6a6cf5f28
Gustavo Adolfo Olmedo <gustavo.olmedo.formosa@gmail.com>
2025-04-12 14:04:35 +0100

    add typeORM and initial setup for pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      password: 'postgres',
+      username: 'postgres',
+      database: 'postgres',
+      synchronize: true,
+      autoLoadEntities: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
