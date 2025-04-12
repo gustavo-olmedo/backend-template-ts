52aa2d9a6c695048428bac591ece7f5d256d3668
Gustavo Adolfo Olmedo <gustavo.olmedo.formosa@gmail.com>
2025-04-12 14:06:57 +0100

    remove useless default controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
@@ -17,7 +15,5 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       logging: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
