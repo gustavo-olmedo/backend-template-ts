faed4ae712014a2706b68e2ad92fcb99afa5c273
Gustavo Adolfo Olmedo <gustavo.olmedo.formosa@gmail.com>
2025-09-26 16:43:09 +0100

    update mail module to include template and mail service providers and export mail service

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,4 +1,6 @@
 import { DynamicModule, Module } from '@nestjs/common';
+import { TemplateService } from './template.service';
+import { MailService } from './mail.service';
 
 export type MailModuleOptions = {
   transport: string;
@@ -14,7 +16,12 @@ export class MailModule {
   static forRoot(options: MailModuleOptions): DynamicModule {
     return {
       module: MailModule,
-      providers: [{ provide: MAIL_OPTS, useValue: options }],
+      providers: [
+        { provide: MAIL_OPTS, useValue: options },
+        TemplateService,
+        MailService,
+      ],
+      exports: [MailService],
     };
   }
 }
