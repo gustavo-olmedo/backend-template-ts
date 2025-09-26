89c36717146bbf72f91941483df51342e712535d
Gustavo Adolfo Olmedo <gustavo.olmedo.formosa@gmail.com>
2025-09-26 16:14:57 +0100

    update mail module to create type and return it WIP

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,4 +1,20 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+
+export type MailModuleOptions = {
+  transport: string;
+  from: string;
+  cache?: boolean;
+  templateDir?: string;
+};
+
+export const MAIL_OPTS = Symbol('MAIL_OPTS');
 
 @Module({})
-export class MailModule {}
+export class MailModule {
+  static forRoot(options: MailModuleOptions): DynamicModule {
+    return {
+      module: MailModule,
+      providers: [{ provide: MAIL_OPTS, useValue: options }],
+    };
+  }
+}
