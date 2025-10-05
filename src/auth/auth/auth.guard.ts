1d10694c3ffa3da926d9f1b2f9fc67e4364d2286
Gustavo Adolfo Olmedo <gustavo.olmedo.formosa@gmail.com>
2025-10-05 18:56:54 +0100

    update auth guard so it includes the user id after veryfing the token

--- src/auth/auth/auth.guard.ts
+++ src/auth/auth/auth.guard.ts
@@ -6,17 +6,22 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { RequestWithId } from '../interfaces/request-with-id.interface';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
-  canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext) {
+    const req = context.switchToHttp().getRequest<RequestWithId>();
     try {
       const request = context.switchToHttp().getRequest();
       const jwt = request.cookies['jwt'];
       if (!jwt) throw new UnauthorizedException();
 
-      return this.jwtService.verify(jwt);
+      const payload = await this.jwtService.verifyAsync(jwt);
+      req.userId = payload.sub;
+
+      return payload;
     } catch (err) {
       console.error('JWT verification error:', err);
       throw new ForbiddenException();
