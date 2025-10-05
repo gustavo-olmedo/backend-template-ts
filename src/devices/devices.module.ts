8020403aca8edb9dcb964f48a2e8cad1b56a1a5c
Gustavo Adolfo Olmedo <gustavo.olmedo.formosa@gmail.com>
2025-10-05 14:53:55 +0100

    add missing import for user and auth within devices module

--- src/devices/devices.module.ts
+++ src/devices/devices.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DevicesController } from './devices.controller';
 import { DevicesService } from './devices.service';
+import { Device } from './models/devices.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Device]), AuthModule, UsersModule],
   controllers: [DevicesController],
-  providers: [DevicesService]
+  providers: [DevicesService],
 })
 export class DevicesModule {}
