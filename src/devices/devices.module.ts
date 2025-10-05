37e28492ced5725f02daa22005fdd443e398d06c
Gustavo Adolfo Olmedo <gustavo.olmedo.formosa@gmail.com>
2025-10-05 15:37:15 +0100

    add missing import for device entity and required modules within devices module

--- src/devices/devices.module.ts
+++ src/devices/devices.module.ts
@@ -5,9 +5,15 @@ import { Device } from './models/device.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import { UsersModule } from 'src/users/users.module';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Device]), AuthModule, UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([Device]),
+    AuthModule,
+    UsersModule,
+    SharedModule,
+  ],
   controllers: [DevicesController],
   providers: [DevicesService],
 })
