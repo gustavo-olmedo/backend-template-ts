313f12f14a27b035c627a77c67ae3438520b9c6c
Gustavo Adolfo Olmedo <gustavo.olmedo.formosa@gmail.com>
2025-10-05 15:36:41 +0100

    add missing services to provider in auth module, & add auth identities to export

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,16 +7,25 @@ import { RolesModule } from '../roles/roles.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PasswordToken } from './models/password-token.entity';
 import { PasswordTokenService } from './password-token.service';
+import { AuthIdentity } from './models/auth-identity.entity';
+import { Session } from './models/session.entity';
+import { AuthIdentitiesService } from './auth-identities.service';
+import { SessionsService } from './sessions.service';
 
 @Module({
   imports: [
     forwardRef(() => UsersModule),
-    TypeOrmModule.forFeature([PasswordToken]),
+    TypeOrmModule.forFeature([PasswordToken, AuthIdentity, Session]),
     SharedModule,
     RolesModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, PasswordTokenService],
-  exports: [AuthService, PasswordTokenService],
+  providers: [
+    AuthService,
+    PasswordTokenService,
+    AuthIdentitiesService,
+    SessionsService,
+  ],
+  exports: [AuthService, PasswordTokenService, AuthIdentitiesService],
 })
 export class AuthModule {}
