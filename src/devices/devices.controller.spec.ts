8d9f5971743c266f9f40a732b284a9568f70c7b0
Gustavo Adolfo Olmedo <gustavo.olmedo.formosa@gmail.com>
2026-07-05 12:00:00 +0100

    add missing dependencies to devices tests

--- src/devices/devices.controller.spec.ts
+++ src/devices/devices.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DevicesController } from './devices.controller';
+import { DevicesService } from './devices.service';
+import { UsersService } from '../users/users.service';
+import { AuthGuard } from '../auth/auth/auth.guard';
 
 describe('DevicesController', () => {
   let controller: DevicesController;
@@ -7,7 +10,14 @@ describe('DevicesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DevicesController],
-    }).compile();
+      providers: [
+        { provide: DevicesService, useValue: {} },
+        { provide: UsersService, useValue: {} },
+      ],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue({ canActivate: () => true })
+      .compile();
 
     controller = module.get<DevicesController>(DevicesController);
   });
--- src/devices/devices.service.spec.ts
+++ src/devices/devices.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DevicesService } from './devices.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Device } from './models/device.entity';
 
 describe('DevicesService', () => {
   let service: DevicesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DevicesService],
+      providers: [
+        DevicesService,
+        {
+          provide: getRepositoryToken(Device),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<DevicesService>(DevicesService);
